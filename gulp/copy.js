f81d00bccd80d73022c47816ae064240c10fb552
KevinAdu <kevyugi@gmail.com>
2017-04-23 13:46:08 +0900

    Added icons for lightgallery

--- gulp/copy.js
+++ gulp/copy.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var sequence = require('run-sequence');
 
 gulp.task('copy', function() {
-  return gulp.src(['src/*images/**/*'])
-  .pipe(gulp.dest('./dist'));
+  return gulp.src(['node_modules/lightgallery.js/dist/fonts/**/*'])
+  .pipe(gulp.dest('./dist/fonts/'));
 });
