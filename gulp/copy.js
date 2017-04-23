d6aee999a05c800bf6c33dd16cec1fdace3d37ac
KevinAdu <kevyugi@gmail.com>
2017-04-23 16:46:58 +0900

    Added favicon

--- gulp/copy.js
+++ gulp/copy.js
@@ -1,7 +1,17 @@
 var gulp = require('gulp');
 var sequence = require('run-sequence');
 
-gulp.task('copy', function() {
+gulp.task('copy:fonts', function() {
   return gulp.src(['node_modules/lightgallery.js/dist/fonts/**/*'])
   .pipe(gulp.dest('./dist/fonts/'));
+});
+
+gulp.task('copy:favicon', function() {
+  return gulp.src(['src/images/favicon.ico'])
+  .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('copy', function() {
+  sequence('copy:fonts',
+           'copy:favicon');
 });
