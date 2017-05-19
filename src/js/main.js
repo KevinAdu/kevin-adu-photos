8e8d6cb62d322590c3b792dcc91daed60a167d59
KevinAdu <kevyugi@gmail.com>
2017-05-19 14:03:38 +0900

    Removed windows load listener

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,24 @@
-(() => {
+(function () {
+  if (document.querySelector('.no-logo a[href*="#my-projects"]')) {
+    var myProjectsLink = document.querySelector('.no-logo a[href*="#my-projects"]');
+    var projectSection = document.getElementById('my-projects');
+
+    myProjectsLink.addEventListener('click', function (e) {
+      e.preventDefault();
+      projectSection.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+
+  if (document.querySelectorAll('.photo')) {
+    var photos = document.querySelectorAll('.photo');
+
+    for (var i = 0; i < photos.length; i++) {
+      var imgAlt = photos[i].querySelector('img').getAttribute('alt');
+      photos[i].dataset.msg = imgAlt;
+    }
+  }
+
   lightGallery(document.getElementById('ul-li'), {
     download: false,
-  });
-
-  window.addEventListener('load', () => {
-    if (document.querySelector('.no-logo a[href*="#my-projects"]')) {
-      const myProjectsLink = document.querySelector('.no-logo a[href*="#my-projects"]');
-      const projectSection = document.getElementById('my-projects');
-
-      myProjectsLink.addEventListener('click', (e) => {
-        e.preventDefault();
-        projectSection.scrollIntoView({ behavior: 'smooth' });
-      });
-    }
-
-    if (document.querySelectorAll('.photo')) {
-      const photos = document.querySelectorAll('.photo');
-
-      for (let i = 0; i < photos.length; i++) {
-        const imgAlt = photos[i].querySelector('img').getAttribute('alt');
-        photos[i].dataset.msg = imgAlt;
-      }
-    }
   });
 })();
